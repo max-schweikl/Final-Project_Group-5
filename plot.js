9f0c2cdce1317e4a559335fbf1a9da277eb7a28c
Stephanie <smongu7@gmail.com>
2022-05-23 18:13:04 -0500

    create plot sheet

---
+++ plot.js
@@ -0,0 +1,13 @@
+// Dropdown menu func "init"
+// function init() {
+    // var selector = d3.select("#selDataset");
+  
+    // d3.json("samples.json").then((data) => {
+    // var sampleNames = data.names;
+    // console.log(data);
+    //   sampleNames.forEach((sample) => {
+    //     selector
+    //       .append("option")
+    //       .text(sample)
+    //       .property("value", sample);
+    //   });
