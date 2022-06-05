47104dd8d7c0746d92a432ff683a54e7e2576ee4
Stephanie <94420548+smongu7@users.noreply.github.com>
2022-06-05 15:25:41 -0500

    complete code for 3/4 of charts

--- charts.js
+++ charts.js
@@ -4,19 +4,16 @@ function init() {
   }
 // INITIALIZE DASHBOARD ///////////////////////////////////////////
 init();
-function optionChanged(newActor) {
+function optionChangedCast(newActor) {
   newGenre = d3.select('#selGenre').node().value
   newCast = d3.select('#selCast').node().value
-  newDirect = d3.select('#selDirector').node().value
-  newComposer = d3.select('#selComposer').node().value
 
   // CHART: Cast
-  d3.json("/getData/"+newCast, {
+  d3.json("/getDataCast/"+newCast, {
     method: 'POST',
     headers: {
         "Content-type": "application/json; charset=UTF-8"
     },
-    // body: JSON.stringify({"actor":newCast})
     body: JSON.stringify(newCast)
 }).then(function (data) {
   console.log(data)
@@ -24,29 +21,66 @@ function optionChanged(newActor) {
   console.log(x_vals)
   y_vals = data.map(y => y.revenue)
   console.log(y_vals)
-// 8. Create the trace for the bar chart.
+// Create the trace for the bar chart.
   var barData = [{
    x: x_vals,
    y: y_vals,
         type: "bar"
       }];
-      // 9. Create the layout for the bar chart.
+      // Create the layout for the bar chart.
       var barLayout = {
         title: "Actor's Successful Movies",
         xaxis: { title: "Movie Titles"},
         yaxis:  { title: "Revenue"}
       };
-      // 10. Use Plotly to plot the data with the layout.
+      // Use Plotly to plot the data with the layout.
       Plotly.newPlot("bar", barData, barLayout);
 })
+}
+function optionChangedDirector(newActor) {
+  newGenre = d3.select('#selGenre').node().value
+  newDirect = d3.select('#selDirector').node().value
+  newComposer = d3.select('#selComposer').node().value
+
+  // CHART: Director
+  d3.json("/getDataDirector/"+newDirect, {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify(newDirect)
+}).then(function (data) {
+  console.log(data)
+  x_vals = data.map(x => x.title)
+  console.log(x_vals)
+  y_vals = data.map(y => y.revenue)
+  console.log(y_vals)
+// Create the trace for the bar chart.
+  var barData = [{
+   x: x_vals,
+   y: y_vals,
+        type: "bar"
+      }];
+      // Create the layout for the bar chart.
+      var barLayout = {
+        title: "Director's Successful Movies",
+        xaxis: { title: "Movie Titles"},
+        yaxis:  { title: "Revenue"}
+      };
+      // Use Plotly to plot the data with the layout.
+      Plotly.newPlot("bar", barData, barLayout);
+})
+};
 
 // CHART: Composer
-d3.json("/getData/"+newComposer, {
+function optionChangedComposer(newActor) {
+  newGenre = d3.select('#selGenre').node().value
+  newComposer = d3.select('#selComposer').node().value
+d3.json("/getDataComposer/"+newComposer, {
   method: 'POST',
   headers: {
       "Content-type": "application/json; charset=UTF-8"
   },
-  // body: JSON.stringify({"actor":newComposer})
   body: JSON.stringify(newComposer)
 }).then(function (data) {
 console.log(data)
@@ -54,22 +88,37 @@ x_vals = data.map(x => x.title)
 console.log(x_vals)
 y_vals = data.map(y => y.revenue)
 console.log(y_vals)
-// 8. Create the trace for the bar chart.
+// Create the trace for the bar chart.
 var barData = [{
  x: x_vals,
  y: y_vals,
       type: "bar"
     }];
-    // 9. Create the layout for the bar chart.
+    // Create the layout for the bar chart.
     var barLayout = {
       title: "Composer's Successful Movies",
       xaxis: { title: "Movie Titles"},
       yaxis:  { title: "Revenue"}
     };
-    // 10. Use Plotly to plot the data with the layout.
+    // Use Plotly to plot the data with the layout.
     Plotly.newPlot("bar", barData, barLayout);
 })
 };
 
-// 1. CREATE buildCharts FUNCTION ///////////////////////////////////////
-// function buildCharts(movies) {
+  // CHART: Genre
+function optionChangedGenre(newActor) {
+  newGenre = d3.select('#selGenre').node().value
+  d3.json("/getDataGenre/"+newGenre, {
+    method: 'POST',
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    },
+    body: JSON.stringify(newGenre)
+  }).then(function (data) {
+    console.log(data[0]['count'])
+  g_count = d3.select("#genreCount").text(data[0]['count'])
+  g_rev = d3.select("#genreRev").text(data[0]['sum'])
+  // g_count = data[0]['count']
+  // g_rev = data[0]['sum']
+})
+};
